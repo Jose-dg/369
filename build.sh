8d9da214006c895a2724c91050368396737e3eb6
José Ojeda <jose.ojeda.k@hotmail.com>
2025-11-16 13:18:23 -0500

    Creación de superusuario

--- build.sh
+++ build.sh
@@ -15,4 +15,7 @@ mkdir -p static
 # Recolecta archivos estáticos
 python manage.py collectstatic --no-input
 
+# Crea el superusuario
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@mail.com', 'admin@mail.com', '9f63B6E14nWjLPtg2fb0O291MnBuWpUhAZ4')" | python manage.py shell
+
 # Otros comandos de preparación si es necesario...
